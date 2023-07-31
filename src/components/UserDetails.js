07d187f90389a496f2a2bb0aaa36327f887ea883
Nderitu Lornah <nderitulornah040@gmail.com>
2023-07-31 17:46:36 +0300

    commit

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -2,40 +2,32 @@ import React, { Component, useEffect, useState } from "react";
 // import AdminHome from "./adminHome";
 
 // import UserHome from "./userHome";
-
-export default function UserDetails() {
-  const [userData, setUserData] = useState("");
-  const [admin, setAdmin] = useState(false);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/userData", {
-      method: "POST",
-      crossDomain: true,
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        token: window.localStorage.getItem("token"),
-      }),
-    })
+componentDidMount(){
+    fetch("http://localhost:3000/userData",{
+        method:"POST",
+        crossDomain:"true",
+        headers:{
+          "Content-type":"application/json",
+          Accept:"application/json",
+          "Access-Control-Allow-Origin":"*"
+        },
+        body: JSON.stringify({
+         token,
+        }),
+      })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data, "userData");
-        if (data.data.userType == "Admin") {
-          setAdmin(true);
-        }
-
-        setUserData(data.data);
-
-        if (data.data == "token expired") {
-          alert("Token expired login again");
-          window.localStorage.clear();
-          window.location.href = "./sign-in";
-        }
-      });
-  }, []);
-
-//   return admin ? <AdminHome /> : <UserHome userData={userData} />;
+        console.log(data, "userRegister");
+      })
+  
+}
+export default class UserDetails extends Component {
+    render() {
+        return(
+            <div>
+                Name<h1>Jayy</h1>
+                Email<h1>Jayy@123</h1>
+            </div>
+        );
+    }
 }
